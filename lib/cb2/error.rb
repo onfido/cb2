11a03c8eb7bb0744c0b2d5c19ccd38b61a91af84
Pedro Silva <pedro.silva@onfido.com>
2016-07-29 10:28:01 +0100

    Add error handling option to the circuit breaker.

--- lib/cb2/error.rb
+++ lib/cb2/error.rb
@@ -1,4 +1,6 @@
 module CB2
+  DEFAULT_ERROR_HANDLER = Proc.new { |error, &post_handle| post_handle.call(error) }
+
   class BreakerOpen < StandardError
   end
 end
--- spec/breaker_spec.rb
+++ spec/breaker_spec.rb
@@ -52,6 +52,71 @@ describe CB2::Breaker do
         end
       end
     end
+
+
+    context "breaker has an error handler function" do
+      let(:breaker) do
+        CB2::Breaker.new(service: "aws",
+          duration: 60,
+          threshold: 5,
+          reenable_after: 600,
+          redis: MockRedis.new,
+          ignore: [RuntimeError, TypeError],
+          error_handler: Proc.new { |e, &h|
+            if (e.is_a?(ZeroDivisionError))
+              h.call(e)
+            else
+              if (e.is_a?(RuntimeError) or e.is_a?(IndexError))
+                raise e
+              else
+                42
+              end
+            end
+          })
+      end
+
+      it "raises errors in the handler even if listed in ignore" do
+        assert_raises(RuntimeError) { breaker.run { raise "sample-rt-error "} }
+      end
+
+      it "does not count errors raised in the handler" do
+        6.times{
+          begin
+            breaker.run { raise 'sample-runtime-error' }
+          rescue
+          end
+        }
+        assert_raises(RuntimeError) { breaker.run { raise "sample-rt-error "} }
+      end
+
+      it "raises non-ignored errors when the handler calls the post-handler" do
+        assert_raises(ZeroDivisionError) { breaker.run { 1 / 0 } }
+      end
+
+      it "counts non-ignored errors when the handler calls the post-handler" do
+        6.times {
+          begin
+            breaker.run { 1 / 0 }
+          rescue
+          end
+        }
+        assert_raises(CB2::BreakerOpen) { breaker.run { 2 } }
+      end
+
+      it "does not count a non-ignored error when the post-handler is not called" do
+        6.times {
+          begin
+            breaker.run { [].fetch(0) }
+          rescue
+          end
+        }
+        assert_raises(IndexError) { breaker.run { [].fetch(0) } }
+      end
+
+      it "ignores an error when the handler does not raise it nor calls the post-handler" do
+        assert_equal 42, breaker.run { [].fetch("4") }
+      end
+    end
   end
 
   describe "#open?" do
